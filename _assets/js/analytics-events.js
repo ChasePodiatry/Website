6ebd606abc283688213af366b3696f0fed55a9bc
Tom Price <tom@codedinternet.com>
2018-03-27 00:13:50 +0100

    Update analytics tracking to gtag.js.
    Add AdWords tracking to better monitor conversions.

--- _assets/js/analytics-events.js
+++ _assets/js/analytics-events.js
@@ -1,21 +1,31 @@
-if(typeof ga === "function") {
+function gtag_report_conversion(url, id) {
+    var callback = function () {
+        if (typeof(url) != 'undefined') {
+            window.location = url;
+        }
+    };
+    gtag('event', 'conversion', {
+        'send_to': id,
+        'event_callback': callback
+    });
+    return false;
+}
+
+if(typeof gtag === "function") {
     $(document).ready(function () {
         $('a[href^="tel:"]').on('click', function () {
-            ga('send', {
-                hitType: 'event',
-                eventCategory: "Contact",
-                eventAction: "tel",
-                eventLabel: this.href.replace(/^tel:/, '')
+            gtag('event', 'tel', {
+                'event_category': "Contact",
+                'event_label': this.href.replace(/^tel:/, '')
             });
+            gtag_report_conversion(this.href, "AW-1007176580/zRguCKSWoQMQhJeh4AM");
         });
 
         $('a[href^="mailto:"]').on('click', function () {
-            ga('send', {
-                hitType: 'event',
-                eventCategory: "Contact",
-                eventAction: "mailto",
-                eventLabel: this.href.replace(/^mailto:/, '')
-            });
+            gtag('event', 'mailto', {
+                'event_category': "Contact",
+                'event_label': this.href.replace(/^mailto:/, '')
+            })
         });
     });
 }
